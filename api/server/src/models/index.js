6eb22b3cdb7b3f07000a00d0080eecc81de86197
laboratoria <monicasayama@gmail.com>
2020-01-31 20:35:44 -0300

    fixed heroku

--- api/server/src/models/index.js
+++ api/server/src/models/index.js
@@ -1,11 +1,14 @@
+'use strict';
+import configJson from "../config/config"
+require('dotenv').config()
 import fs from 'fs'
 import path from 'path'
 import Sequelize from 'sequelize'
 
-require('dotenv').config()
-const config = require(__dirname + '/../config/config.js')[env];
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '../config/config.js')[env];
+const config = configJson[env]
 
 const db = {}
 
