3eb8ab426c32e46f7d1c2fbc3dc3c685e20c1d4b
Monica Yamazaki <monicasayama@gmail.com>
2020-02-01 23:57:31 -0300

    add itens order no seed

--- api/server/src/seeders/20200130203459-itensOrder.js
+++ api/server/src/seeders/20200130203459-itensOrder.js
@@ -7,8 +7,26 @@ module.exports = {
         productId: 2,
         orderId: 1,
         additionalId: 5,
-        qtd: 2,
+        qtd: 1,
         option: "vegetariano",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        productId: 2,
+        orderId: 1,
+        additionalId: 5,
+        qtd: 1,
+        option: "bovino",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        productId: 2,
+        orderId: 3,
+        additionalId: 5,
+        qtd: 1,
+        option: "frango",
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -22,13 +40,41 @@ module.exports = {
       {
         productId: 1,
         orderId: 2,
+        qtd: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        productId: 3,
+        orderId: 4,
+        qtd: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        productId: 3,
+        orderId: 5,
+        qtd: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        productId: 4,
+        orderId: 6,
+        qtd: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        productId: 4,
+        orderId: 7,
         qtd: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         productId: 3,
-        orderId: 3,
+        orderId: 7,
         qtd: 2,
         createdAt: new Date(),
         updatedAt: new Date()
